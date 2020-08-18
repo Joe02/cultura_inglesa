ba9248ba801a4198511a084d6981537465e9e930
joe02 <joe02araujo@gmail.com>
2020-08-18 09:56:23 -0300

    Created first prototype for login screen using Custom Input forms widgets.

--- lib/pages/login_screen_page.dart
+++ lib/pages/login_screen_page.dart
@@ -1,5 +1,7 @@
+import 'package:cultura_inglesa/utils/custom_form_input.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class LoginScreenPage extends StatefulWidget {
   @override
@@ -7,8 +9,109 @@ class LoginScreenPage extends StatefulWidget {
 }
 
 class LoginScreenPageState extends State<LoginScreenPage> {
+  final _userController = TextEditingController();
+  final _passwordController = TextEditingController();
+  var _obscurePassword = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width / 5,
+            ),
+            child: Card(
+              color: Colors.grey[100],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25))),
+              child: Column(
+                children: <Widget>[
+                  CustomFormInput("Usuário", "Digite seu usuário",
+                      Icons.supervised_user_circle, "Text"),
+                  CustomFormInput("Senha", "Digite sua senha",
+                      Icons.lock, "Password"),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          right: 15.0, top: 15.0, bottom: 5.0),
+                      child: Text(
+                        "Esqueceu sua senha?",
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.blue[600],
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 10),
+                            child: RaisedButton(
+                              elevation: 4,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(25))),
+                              color: Colors.blueAccent,
+                              onPressed: () {},
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10.0),
+                                child: Text(
+                                  "Continuar",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      letterSpacing: 1),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      //TODO Navigate to Register page
+                    },
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom: 20),
+                        child: RichText(
+                          text: TextSpan(
+                            text: "Não possui uma conta?",
+                            style: TextStyle(
+                                fontSize: 17, color: Colors.grey[800]),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: " Crie uma agora!",
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      color: Colors.blue,
+                                      fontWeight: FontWeight.bold))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/custom_form_input.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomFormInput extends StatefulWidget {
+  final title;
+  final hint;
+  final icon;
+  final type;
+
+  CustomFormInput(this.title, this.hint, this.icon, this.type);
+
+  @override
+  CustomFormInputState createState() => CustomFormInputState();
+}
+
+class CustomFormInputState extends State<CustomFormInput> {
+  final _customInputController = TextEditingController();
+  var _obscureText = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 15,
+      ),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  widget.title,
+                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.w500),
+                )),
+          ),
+          Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(25))),
+            elevation: 5,
+            child: TextFormField(
+              obscureText: _obscureText,
+              onChanged: (value) {
+                setState(() {});
+              },
+              controller: _customInputController,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                hintText: widget.hint,
+                hintStyle: TextStyle(fontSize: 19, color: Colors.grey),
+                contentPadding: EdgeInsets.only(top: 12.5),
+                prefixIcon: Icon(widget.icon,
+                    color: _customInputController.text == ""
+                        ? Colors.grey[600]
+                        : Colors.blue),
+                suffixIcon: GestureDetector(
+                    onTap: () {
+                      if (widget.type == "Text") {
+                        setState(() {
+                          _customInputController.clear();
+                        });
+                      } else {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      }
+                    },
+                    child: widget.type == "Text"
+                        ? Icon(Icons.clear,
+                            color: _customInputController.text == ""
+                                ? Colors.grey[600]
+                                : Colors.blue)
+                        : Icon(
+                            _obscureText
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: _customInputController.text == ""
+                                ? Colors.grey[600]
+                                : Colors.blue)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
